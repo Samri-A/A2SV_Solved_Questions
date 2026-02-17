42e52180e91d3119be83f824111f9400a3c5119b
Samrawit Asfaw <146061887+Samri-A@users.noreply.github.com>
2026-02-17 14:16:40 +0300

    Time: 0 ms (100.00%) | Memory: 12.4 MB (89.87%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        
+        
+        value_symbols = [
+        (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+        (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'),
+        (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+         ]
+        answer = ""
+        for value , key  in value_symbols:
+           if num == 0:
+               break
+           result = num // value
+           num = num % value
+           answer += key * result
+        return answer
+
