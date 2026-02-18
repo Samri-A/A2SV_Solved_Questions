11a52d0991f93d8fcaf9863f8f03784aecd7b04f
Samrawit Asfaw <146061887+Samri-A@users.noreply.github.com>
2026-02-18 05:12:20 +0300

    Create C_Dominant_Character.py

---
+++ C_Dominant_Character.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+for i in range(t): 
+    n = int(input())
+    st = input()
+    
+    if "aa" in st: 
+        print(2)
+    elif "aba" in st or "aca" in st: 
+        print(3)
+    elif "abca" in st or "acba" in st: 
+        print(4)
+    elif "abbacca" in st or "accabba" in st: 
+        print(7)
+    else: 
+        print(-1)
