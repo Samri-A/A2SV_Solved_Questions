14125016616849454e2727ed9c578ae17fbbc9c8
Samrawit Asfaw <146061887+Samri-A@users.noreply.github.com>
2026-02-19 19:26:42 +0300

    Time: 693 ms (7.02%) | Memory: 21.6 MB (15.79%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -1,13 +1,6 @@
 class Solution:
     def smallestPalindrome(self, s: str) -> str:
-
-        # for ex : daccad. consider the length also when it is even and odd
-        # first letter = a
-        # if it is odd there must be a letter that is not a pair ( like aca : c is one) so we put that letter in the middle 
-        # and we will append the next lexicographic charachter 
-        # """is not it better if we sort them first so it will be easy to find""" what about using min to find the smallest lexical one 
-        
-                
+         
         sorted_s = "".join(sorted(s))
         first_mirror = ""
         oddstr=""
@@ -31,7 +24,7 @@ class Solution:
                         first_mirror += key * ((odd_freq[key]- 1)//2) 
                         middle = key
                     else:
-                         middle = key #we have to make this odd str cause it is the last part malt ok chershi ena i will let you know wht i am thinking
+                         middle = key 
                     
                 
         print(first_mirror)
@@ -40,5 +33,5 @@ class Solution:
        
         
         if len(sorted_s)%2 == 0:
-            return  first_mirror+mirrored    #yeah i am trying to sort it kelay but forgot the syntax
+            return  first_mirror+mirrored    
         else:  return  first_mirror + middle + mirrored
