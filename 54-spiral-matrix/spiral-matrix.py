0936ff245262b2ec4df6d76887caea779816292e
Samrawit Asfaw <146061887+Samri-A@users.noreply.github.com>
2026-02-23 09:26:38 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (75.91%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.py
@@ -0,0 +1,36 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        track_seen = {}
+        answer = []
+        top = 0
+        left = 0
+
+        # m*n matrix 
+        bottom = len(matrix)
+        right = len(matrix[0])
+
+        while left < right and top < bottom:
+
+            for i in range(left , right):
+                answer.append(matrix[top][i])
+            
+            top+=1
+    
+            for i in range(top , bottom):
+                answer.append(matrix[i][right-1])
+            right-=1
+
+            if not (left < right and top < bottom):
+                break
+
+
+            for i in range(right-1, left-1, -1):
+                answer.append(matrix[bottom-1][i])
+    
+            bottom -= 1
+
+            for i in range( bottom-1 , top-1 , -1):
+                answer.append(matrix[i][left])
+
+            left+=1
+        return answer
