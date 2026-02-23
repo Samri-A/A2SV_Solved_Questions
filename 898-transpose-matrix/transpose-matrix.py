f7c3143db33ebfac4ae94456df688a4925be1a0e
Samrawit Asfaw <146061887+Samri-A@users.noreply.github.com>
2026-02-23 06:31:55 +0300

    Update transpose-matrix.py

--- 898-transpose-matrix/transpose-matrix.py
+++ 898-transpose-matrix/transpose-matrix.py
@@ -1,11 +1,11 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        pointer_row = 0
-        pointer_column = 0
+        # Create new matrix
         row , column = len(matrix) , len(matrix[0])
         transpose = [[0] * row for _ in range(column)]
         print(transpose)
-        
+
+        #Tranverse the postion for each cell 
         for r in range(len(matrix)):
             for c in range(len(matrix[0])):
                 transpose[c][r] = matrix[r][c]
