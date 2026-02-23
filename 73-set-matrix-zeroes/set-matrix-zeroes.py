4bb32d256576b5007960eeac52212a49b708b105
Samrawit Asfaw <146061887+Samri-A@users.noreply.github.com>
2026-02-23 07:26:54 +0300

    Update set-matrix-zeroes.py

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        # Get all the column and row number of zeros
         columns = {}
         rows = {}
         for row in range(len(matrix)):
@@ -14,13 +15,14 @@ class Solution:
                     if col not in columns:
                         columns[col] = 1
 
+        # make all the columns of selected value zero
         for key in columns:
             for row in range(len(matrix)):
                 matrix[row][key] = 0
 
+        # make all the rows of selected value zero
         for key in rows:
             for col in range(len(matrix[0])):
                 matrix[key][col] = 0
 
-        print( rows , columns )
-
+        
