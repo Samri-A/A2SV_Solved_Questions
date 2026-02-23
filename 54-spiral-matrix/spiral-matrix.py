020325b9bcfedb00d68736805476c471eeb613a3
Samrawit Asfaw <146061887+Samri-A@users.noreply.github.com>
2026-02-23 09:30:29 +0300

    Update spiral-matrix.py

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -5,30 +5,36 @@ class Solution:
         top = 0
         left = 0
 
-        # m*n matrix 
+
         bottom = len(matrix)
         right = len(matrix[0])
 
         while left < right and top < bottom:
 
+            # Go through top
             for i in range(left , right):
                 answer.append(matrix[top][i])
             
             top+=1
     
+             # Go through right
             for i in range(top , bottom):
                 answer.append(matrix[i][right-1])
             right-=1
 
+
+            #  Break before going inward if it reached
             if not (left < right and top < bottom):
                 break
+            
 
-
+            #  Go through bottom
             for i in range(right-1, left-1, -1):
                 answer.append(matrix[bottom-1][i])
     
             bottom -= 1
 
+             # Go through the left
             for i in range( bottom-1 , top-1 , -1):
                 answer.append(matrix[i][left])
 
