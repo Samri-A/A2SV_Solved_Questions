42b40217d3194c36f29ef3ae31c52af80544e0e5
Samrawit Asfaw <146061887+Samri-A@users.noreply.github.com>
2026-02-24 18:09:25 +0300

    Update divide-players-into-teams-of-equal-skill.py

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -1,18 +1,18 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
-        # sum_of_skills = 0
-        # for i in skill:
-        #     sum_of_skills+=i
-
-        # # Check if we can divde the skills equally
-        # if sum_of_skills%2 != 0:
-        #     return -1
+       
 
         sum_of_team_chemistry = 0
         left = 0
         right = len(skill) - 1
+
+        # Sort to make matching 
         skill.sort()
+
+        # get one valid match
         match = skill[left] + skill[right]
+
+        # check if each pair equals to the match and return the team chemistry 
         while left < right:
             if skill[left] + skill[right] == match:
                 sum_of_team_chemistry += skill[left] * skill[right]
