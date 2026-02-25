1b0346cd35f3eff15611034b4ac4af6e4321ab43
Samrawit Asfaw <146061887+Samri-A@users.noreply.github.com>
2026-02-25 04:52:50 +0300

    Clean up commented code in sum_of_square_numbers.py
    Removed commented-out code for clarity.

--- 633-sum-of-square-numbers/sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -13,30 +13,6 @@ class Solution:
             b = c - a**2
             if b in possibe_b_squared:
                 return True
-        # nums = []
-        # for i in range(1, num+1):
-           
-        #     nums.append(i)
-
-        
-        # if len(nums) == 1:
-        #     if nums[0] ** 2 + nums[0] ** 2== c or  nums[0] ** 2 == c:
-        #         return True
-
-                
-        # left = 0
-        # right = len(nums) - 1
-        # mid = len(nums)//2
-        # while left < right:
-        #     temp = nums[left] ** 2 + nums[right] ** 2
-        #     if temp == c or nums[left] ** 2 == c or nums[right] ** 2 == c or nums[mid] ** 2 == c:
-        #         return True
-        #     elif nums[left] ** 2 + nums[mid] ** 2 > c:
-        #         left+=1
-        #         right = mid
-        #     else:
-        #         left = mid
-        #         right -= 1
 
         
         
