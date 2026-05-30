5610d4e40c157ffda956711e2c810d0ef4645bb4
Samrawit Asfaw <146061887+Samri-A@users.noreply.github.com>
2026-05-30 06:35:45 +0300

    Time: 65 ms (34.93%), Space: 20.9 MB (22.01%) - LeetHub

--- 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.py
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.py
@@ -1,15 +1,11 @@
 class Solution:
     def maxCount(self, banned: list[int], n: int, maxSum: int) -> int:
-        banned_set = set(banned)  # Use a set for quick lookups
-        total_sum = 0  # Track the cumulative sum
-        count = 0  # Track the count of valid numbers
-
-        for i in range(1, n + 1):
-            if i in banned_set:
-                continue  # Skip banned numbers
-            total_sum += i
-            if total_sum > maxSum:
-                break  # Stop if the sum exceeds maxSum
-            count += 1
+        choosen = set(banned)
+        sum_ = 0
+        count = 0
+        for i in range(1, n+1):
+            if i not in choosen and (sum_ + i) <= maxSum:
+                count += 1
+                sum_ += i
 
         return count
