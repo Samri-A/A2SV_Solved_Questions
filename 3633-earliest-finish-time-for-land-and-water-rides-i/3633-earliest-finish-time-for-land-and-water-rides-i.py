99bf3511f9971f034b0a6442b317709c3e7caec7
Samrawit Asfaw <146061887+Samri-A@users.noreply.github.com>
2026-06-03 00:08:52 +0300

    Time: 242 ms (32.37%), Space: 19.4 MB (57.8%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
@@ -1,23 +1,19 @@
 class Solution:
-    def calFinishTime(self, ls, ld, ws, wd):
-
-        mini = float('inf')
-
-        for i in range(len(ls)):
-            mini = min(mini, ls[i] + ld[i])
-
-        ans = float('inf')
-
-        for i in range(len(ws)):
-            ans = min(
-                ans,
-                max(mini, ws[i]) + wd[i]
-            )
-
-        return ans
-
     def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
-        return min(
-            self.calFinishTime(landStartTime,landDuration,waterStartTime,waterDuration),
-            self.calFinishTime(waterStartTime,waterDuration,landStartTime,landDuration)
-        )
+        
+
+        min_possible_time = float('inf')
+
+        for i in range(len(landStartTime)):
+            end = landStartTime[i] + landDuration[i]
+            for j in range(len(waterStartTime)):
+                ans = max(waterStartTime[j] ,end ) + waterDuration[j]  
+                min_possible_time = min(ans , min_possible_time)
+
+        for i in range(len(waterStartTime)):
+            end = waterStartTime[i] + waterDuration[i]
+            for j in range(len(landDuration)):
+                    ans = max(landStartTime[j] , end )+landDuration[j] 
+                    min_possible_time = min(ans , min_possible_time)
+
+        return min_possible_time
