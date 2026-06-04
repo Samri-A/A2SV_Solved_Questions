27ab918f4c858462236f430aefda23c7b2fd121c
Samrawit Asfaw <146061887+Samri-A@users.noreply.github.com>
2026-06-04 10:38:34 +0300

    Time: 128 ms (50.51%) | Memory: 35 MB (46.07%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.py
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.py
@@ -1,23 +1,19 @@
 class Solution:
-    def calFinishTime(self, ls, ld, ws, wd):
+    def helper( self , la , ld , wa , wd):
 
-        mini = float('inf')
-
-        for i in range(len(ls)):
-            mini = min(mini, ls[i] + ld[i])
-
+        mine = float('inf')
+        for i in range(len(la)):
+            mine = min(mine , la[i] + ld[i])
+        
         ans = float('inf')
-
-        for i in range(len(ws)):
-            ans = min(
-                ans,
-                max(mini, ws[i]) + wd[i]
-            )
+        for i in range(len(wa)):
+            ans = min(ans ,max( mine , wa[i])+wd[i])
 
         return ans
 
+
+
+
     def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
-        return min(
-            self.calFinishTime(landStartTime,landDuration,waterStartTime,waterDuration),
-            self.calFinishTime(waterStartTime,waterDuration,landStartTime,landDuration)
-        )
+        return min(self.helper(landStartTime ,landDuration , waterStartTime , waterDuration) , self.helper(waterStartTime ,waterDuration, landStartTime , landDuration) )
+        
