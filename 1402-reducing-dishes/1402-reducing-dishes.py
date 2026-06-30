6e6b2fd28850137031a7ade39bfeecf60728a5f7
Samrawit Asfaw <146061887+Samri-A@users.noreply.github.com>
2026-06-30 17:41:21 +0300

    Time: 9 ms (38.91%), Space: 19.2 MB (66.93%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -3,14 +3,9 @@ class Solution:
         satisfaction.sort()
         n = len(satisfaction)
         max_time = 0 
-
-        for i in range(n):
-            time = 1
-            cont = 0
-            for j in range(i, n):
-                cont += (time * satisfaction[j])
-                time+=1
-                
-                max_time = max(max_time  , cont)
+        sufix = 0
+        for i in range(n-1 , -1, -1):
+            sufix = satisfaction[i] + sufix + sum(satisfaction[i+1:])
+            max_time = max(max_time  , sufix)
 
         return max_time
