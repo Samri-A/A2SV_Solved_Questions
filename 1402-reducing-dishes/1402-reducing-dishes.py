98c7b487c3b605b400a569d4a3ab142b8c1c8176
Samrawit Asfaw <146061887+Samri-A@users.noreply.github.com>
2026-06-30 17:45:38 +0300

    Time: 4 ms (44.36%), Space: 19.3 MB (38.52%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -4,8 +4,10 @@ class Solution:
         n = len(satisfaction)
         max_time = 0 
         sufix = 0
+        score = 0
         for i in range(n-1 , -1, -1):
-            sufix = satisfaction[i] + sufix + sum(satisfaction[i+1:])
-            max_time = max(max_time  , sufix)
+            sufix += satisfaction[i] 
+            score += sufix
+            max_time = max(max_time  , score)
 
         return max_time
